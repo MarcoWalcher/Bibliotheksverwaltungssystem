661d320758696993359cd7d36799663b4bf25652
MarcoWalcher <119504368+MarcoWalcher@users.noreply.github.com>
2025-02-07 12:35:41 +0100

    Created Project
    Created Mavenproject with Netbeans

---
+++ src/main/java/marco_walcher/bibliotheksverwaltungssystem/Bibliotheksverwaltungssystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package marco_walcher.bibliotheksverwaltungssystem;
+
+/**
+ *
+ * @author Omen
+ */
+public class Bibliotheksverwaltungssystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
